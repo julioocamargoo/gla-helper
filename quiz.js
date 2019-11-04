dd11434aa4413f98cb4bcb328f0fd43c2b8c0a62
Mateus Mangerona <mateusmangerona@Mateuss-MacBook-Pro.local>
2019-11-04 15:54:30 -0300

    feeding the bank part 1

--- quiz.js
+++ quiz.js
@@ -1,10 +1,71 @@
 var quiz = [
     {
-        "question": "Ace nasceu em Beritella",
+        "question": "A habilidade Liberation do Marshaal D. Teach. tem dois segundos de tempo de recarga e 20 de poder",
         "answer": "green"
     },
     {
-        "question": "Ace morreu em amsterdam",
+        "question": "A rotacao semanal muda toda sexta-feira",
+        "answer": "green"
+    },
+    {
+        "question": "A rotacao semanal muda todo sabado",
         "answer": "red"
     },
+    {
+        "question": "Anel da cruz é um item recomendado para o nível 50",
+        "answer": "red"
+    },
+    {
+        "question": "Atualmente Pearl é o unico personagem com tres classes-Tanque,Especialista e Lutador",
+        "answer": "red"
+    },
+    {
+        "question": "Atum e a melhor comida que voce pode construir",
+        "answer": "green"
+    },
+    {
+        "question": "Baratie é o primeiro local em que voce pode construir um navio nivel 3",
+        "answer": "green"
+    },
+    {
+        "question": "Baratie é recomendado para jogadores do level 42 a 50 no minimapa do jogo",
+        "answer": "red"
+    },
+    {
+        "question": "Bartholomew Kuma le sua biblia durante a animação do seu emote",
+        "answer": "green"
+    },
+    {
+        "question": "Bastao do rei macaco é um item que pode ser usado apenas por lutadores",
+        "answer": "red"
+    },
+    {
+        "question": "Boa Hancock nao pode petrificar o Monkey D. Luffy",
+        "answer": "green"
+    },
+    {
+        "question": "Buchi pode usar a habilidade take the lead para chamar Sham para lutar ao lado dele",
+        "answer": "red"
+    },
+    {
+        "question": "Calça de tritao é o item mais caro que o genzo vende e custa 10000 berry",
+        "answer": "red"
+    },
+    {
+        "question": "Capone gang bege é o unico personagem que pode convocar dois subordinados sem esperar o tempo de regarga",
+        "answer": "green"
+    },
+    {
+        "question": "Capone gang bege e seus aliados ficam imunes dentro do big father",
+        "answer": "red"
+    },
+    {
+        "question": "Carne de porco goumet é a comida mais demorada para cozinhar do barati. demora 8 minutos",
+        "answer": "red"
+    },
+    {
+        "question": "Daddy mcshooty é um inimigo em loguetown",
+        "answer": "red"
+    },
+    
 ]
